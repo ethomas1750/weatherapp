3676b34ff57796ddcda1eee01d8ef25205a3033f
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-09 23:01:48 -0500

    Update weatherRouter.js

--- weatherRouter.js
+++ weatherRouter.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
 
+const {getAllLocales, addLocale, deleteLocaleById} = require('./locations/controller/weatherController')
+
+router.get('/get-all-searched-locations', getAllLocales);
+
+router.delete('/delete-location/:id', deleteLocaleById)
+
+router.post('/add-location', addLocale)
+
+
+module.exports = router
