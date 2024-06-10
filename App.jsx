fd8f444d1f3592f4361f02a4aeb122494974eba7
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-09 23:06:18 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,22 @@
+import React, { Component } from 'react'
+import Home from './components/Home/Home'
+import './App.css'
+import Header from './components/Header/Header'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
 
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <ToastContainer position='top-center'/>
+        <Header/>
+        <Home/>
+      </div>
+    )
+  }
+}
+
+export default App
